0f26fae0941523ec5791425b2eed6ef9cbf93298
rishabhm74 <rishabhm1974@gmail.com>
2020-07-11 23:55:03 +0530

    first run

--- static/script.js
+++ static/script.js
@@ -5,12 +5,16 @@ document.getElementById("sideMenuOpen").addEventListener("click", ()=>{
         document.body.style.marginLeft = "70%";
         document.body.style.transition = "0.7s";
         document.getElementById("sideMenu").style.transform = "transformX(70%)";
+        document.getElementById("sideMenuOpenIcon").style.transform = "rotate(90deg)";
+        document.getElementById("sideMenuOpenIcon").src = "static/assets/cross.svg";
         sideMenuChecker = sideMenuChecker + 1;
     }
     else{
         document.body.style.marginLeft = "0%";
         document.body.style.transition = "0.7s";
         document.getElementById("sideMenu").style.marginLeft = "-70%";
+        document.getElementById("sideMenuOpenIcon").style.transform = "rotate(0deg)";
+        document.getElementById("sideMenuOpenIcon").src = "static/assets/menu.svg";
         sideMenuChecker = sideMenuChecker - 1;
     }
 });
